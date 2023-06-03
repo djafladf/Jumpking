345dfcc757814732aa9f7698153249b191a7a33c
djafladf <djafladf@naver.com>
2023-06-04 03:43:04 +0900

    긑

--- aa.py
+++ aa.py
@@ -1,14 +1,20 @@
-#Web vpython 3.2
 from vpython import *
 
 def on_keydown(event):
     if not(player.IsJump or player.WaitForJump):
         if event.key == "left":
             player.dx = -1
-            player.MoveVert = True
+            if player.IsSliding:
+                player.v.x = player.v.x - 0.5
+            else:
+                player.MoveVert = True
         elif event.key == "right":
             player.dx = 1
-            player.MoveVert = True
+            if player.IsSliding:
+                player.v.x = player.v.x + 0.5
+            else:
+                player.dx = 1
+                player.MoveVert = True
         elif event.key == " " and not player.MoveVert:
             player.WaitForJump = True
             player.size.y = player.size.y * 0.5
@@ -21,7 +27,8 @@ def on_keyup(event):
         player.WaitForJump = False
         player.IsJump = True
         player.OnLand = False
-        player.v = vec(player.dx * 0.5 * player.JumpP,player.JumpP,0)
+        player.IsSliding = False
+        player.v = vec(player.v.x + player.dx * 0.5 * player.JumpP,player.JumpP,0)
         player.color = color.red
         player.JumpP = 0
     elif event.key == "left" or event.key == "right":
@@ -29,6 +36,7 @@ def on_keyup(event):
         
 
 def DetectCollision():
+    global Game
     l = 0
     for i in CurObjects:
         # Box
@@ -39,9 +47,13 @@ def DetectCollision():
             elif(z== 2):
                 l = 1
         # Triangle
-        else:
+        elif i.type == 1 and not player.OnLand:
             if(Collision_Tri(i)):
                 return True
+        elif i.type == 2:
+            if mag(i.pos - player.pos) <= 1:
+                Game = False
+                print("Clear!")
     if l == 1:
         return True
     return False
@@ -58,15 +70,23 @@ def Collision_Box(Object):
         return 0
     
     if OverlapY == 0 or OverlapX == 0:
+        if not player.IsSliding and Object.friction != 0:
+            player.color = color.blue
+            player.IsSliding = True
+            player.v.x = player.dx * 5 * Object.friction
         return 2
     
     if OverlapX < OverlapY:
         if player.IsJump:
             if player.v.x != 0:
-                player.v.x = player.dx * player.v.x * 0.8
                 player.dx = -player.dx
+                player.v.x = player.dx * abs(player.v.x) * 0.8
+                
         elif player.dx * xsub >= 0:
-            player.pos.x = Object.pos.x - (Object.size.x + player.size.x * 1.4) * 0.5 * player.dx
+            player.pos.x = Object.pos.x - (Object.size.x + player.size.x * 1.5) * 0.5 * player.dx
+            player.color = color.white
+            player.v.x = 0
+            
             
     else:
         if player.pos.y > Object.pos.y:
@@ -74,7 +94,10 @@ def Collision_Box(Object):
             player.IsJump = False
             player.OnLand = True
             player.color = color.white
-            player.v.x = 0
+            player.v.x = player.v.x * Object.friction
+            if player.v.x != 0:
+                player.color = color.blue
+                player.IsSliding = True
         else:
             player.pos.y = Object.pos.y - (Object.size.y+player.size.y) * 0.5
         player.v.y = 0
@@ -99,17 +122,14 @@ def Collision_Tri(tri):
     if cnty[0] > RYmax or cnty[2] < RYmin:
         return False
     
-    if tri.IsReverse:
-        j2 = player.pos.y < tri.line[0].y
-        if tri.l > 0:
-            j = RXmin >= tri.line[0].x and RXmax <= tri.line[1].x
-        else:
-            j = RXmin >= tri.line[1].x and RXmax <= tri.line[0].x
-    else:
+    if not tri.IsReverse:
         j2 = player.pos.y > tri.line[0].y
-        j = True
-        
-    if j2 and j:
+    else:
+        j2 = player.pos.y < tri.line[0].y
+    
+    if j2:
+        if not tri.IsReverse and player.v.y > 0:
+            return False
         k = abs(-player.pos.x * tri.l + player.pos.y - tri.c) / sqrt(player.pos.x ** 2 + player.pos.y ** 2)
         if k > 0.005:
             return False
@@ -120,11 +140,10 @@ def Collision_Tri(tri):
         ang = diff_angle(tri.line[1] - tri.line[0],vec(1,0,0))
         grav = norm(tri.line[0] - tri.line[1]) * sin(ang) * 9.8
         player.rotate(angle = ang,axis = vec(0,0,1))
-        if player.dx * tri.l > 0:
-            player.dx = -1
+        if player.dx * tri.l >= 0:
+            player.dx = -player.dx
             player.v = vec(0,0,0)
         else:
-            player.dx = 1
             player.v = vec(player.v.x,player.v.x * grav.y / grav.x,0)
         while player.pos.y > tri.line[0].y:
             rate(1/dt)
@@ -132,14 +151,24 @@ def Collision_Tri(tri):
             player.pos = player.pos + player.v * dt
         player.rotate(angle = -ang,axis = vec(0,0,1))
     else:
-        player.v.x = -player.v.x
-        player.v.y = -1
+        if tri.l < 0 and tri.line[1].y >= player.pos.y:
+            player.v.y = -1
+        elif tri.l >0 and tri.line[0].y >= player.pos.y:
+            player.v.y = -1
+        else:
+            player.v.x = -player.v.x
+            player.pos.x = player.v.x * dt
     return True
 
 def BoxInit(x,y,sx,sy,cl,fr,st):
     a = box(pos = vec(x,y+st*30,0), size = vec(sx,sy,0.001), color = cl)
     a.friction = fr
     a.type = 0
+    return a
+    
+def EndInit(x,y,sx,sy):
+    a = box(pos = vec(x,y+9*30,0), size = vec(sx,sy,0.001), color = RGB(255,192,203))
+    a.type = 2
     return a
 
 def TriInit(a,b,c,cl,st):
@@ -174,7 +203,6 @@ def TriInit(a,b,c,cl,st):
 
 def StageChange(ch):
     if player.CurStage == len(Stages)-1 and ch == 1:
-        print("Clear")
         return
     player.CurStage = player.CurStage + ch
     scene.center = vec(0,player.CurStage * 30,0)
@@ -186,8 +214,8 @@ def StageChange(ch):
 def RGB(r,g,b):
     return vec(r/255,g/255,b/255)
 
-scene.width = 1600
-scene.height = 1200
+scene.width = 800
+scene.height = 600
 scene.center = vec(0,0,0)
 scene.range = 15
 
@@ -198,11 +226,13 @@ c3 = RGB(66,66,64)
 c4 = RGB(64,35,31)
 c5 = RGB(41,66,67)
 c6 = RGB(56,42,48)
+c7 = RGB(85,159,239)
+c8 = RGB(77,76,81)
 
 CurObjects = [BoxInit(20.5,0,1,1000,color.black,0,0),BoxInit(-20.5,0,1,1000,color.white,0,0)]
-StageBG = [RGB(71,102,66),RGB(71,102,66),RGB(110,149,166),RGB(110,149,166),RGB(110,149,166),RGB(56,53,53),RGB(56,53,53)]
+StageBG = [RGB(71,102,66),RGB(71,102,66),RGB(110,149,166),RGB(110,149,166),RGB(110,149,166),RGB(56,53,53),RGB(56,53,53),RGB(56,53,53),RGB(0,0,0),RGB(246,221,69)]
 
-Stage1 = [BoxInit(0,-14.5,40,2,c1,0,0),BoxInit(15,-7,10,14,c1,0,0),BoxInit(-15,-7,10,14,c1,0,0),BoxInit(0,10,10,4,c0,0,0)]
+Stage1 = [BoxInit(0,-14.5,40,2,c7,0.6,0),BoxInit(15,-7.5,10,12,c1,0,0),BoxInit(-15,-7.5,10,12,c1,0,0),BoxInit(0,10,10,4,c0,0,0)]
 Stage2 = [BoxInit(9,-12,10,3,c0,0,1),BoxInit(18,-4,4,3,c0,0,1),BoxInit(4,-4,6,3,c0,0,1),BoxInit(-8,2,6,6,c0,0,1),BoxInit(-17,4,6,8,c0,0,1)]
 Stage3 = [BoxInit(0,-12,4,1.5,c2,0,2),BoxInit(9,-12,5,1.5,c2,0,2),BoxInit(18.5,-8,3,1.5,c2,0,2),BoxInit(1,-6,10,3,c0,0,2),BoxInit(8,-5.5,4,4,c0,0,2)]
 Stage3.extend([BoxInit(-5,3,5,4,c0,0,2),BoxInit(-17.5,7,5,1.5,c2,0,2),BoxInit(-7,14,6,2,c0,0,2)])
@@ -214,8 +244,12 @@ Stage6 = [BoxInit(0,-13,16,4,c4,0,5),BoxInit(12.5,1,15,12,c5,0,5),BoxInit(-8,-4.
 Stage6.extend([BoxInit(-12,14.25,16,1.5,c5,0,5),BoxInit(-8.5,5.75,9,1.5,c5,0,5),BoxInit(10.5,13.5,11,3,c5,0,5)])
 Stage7 = [BoxInit(8,-8.5,6,13,c5,0,6),BoxInit(13.5,-14.25,5,1.5,c5,0,6),BoxInit(13.5,-2.75,5,1.5,c5,0,6),BoxInit(16,10,8,10,c5,0,6),TriInit(vec(12,14,0),vec(12,5,0),vec(5,5,0),c5,6)]
 Stage7.extend([BoxInit(-3,10,6,10,c5,0,6),BoxInit(-5.5,-6,5,1.5,c6,0,6),TriInit(vec(-4,-15,0),vec(-11,-15,0),vec(-11,-8,0),c5,6),TriInit(vec(-11,-6,0),vec(-18.5,-6,0),vec(-18.5,2,0),c5,6)])
-
-Stage8 = []
+Stage7.extend([BoxInit(-19.25,1,1.5,2,c5,0,6),BoxInit(-6.75,10,1.5,2,c6,0,6),TriInit(vec(-7.5,11,0),vec(-7.5,9,0),vec(-9.5,9,0),c6,6),BoxInit(-16,14.25,8,1.5,c5,0,6)])
+Stage8 = [BoxInit(-3,-11,6,8,c5,0,7),BoxInit(-16,-14.25,8,1.5,c5,0,7),BoxInit(18,-10,4,10,c5,0,7),TriInit(vec(11,-15,0),vec(16,-15,0),vec(16,-5,0),c5,7)]
+Stage8.extend([BoxInit(-10,0,20,4,c6,0,7),BoxInit(-6,12,12,6,c5,0,7),BoxInit(-19,6,2,8,c5,0,7)])
+Stage9 = [BoxInit(-18.5,-13,3,1.5,c7,0.4,8),BoxInit(-6,-14,12,2,c7,0.4,8),TriInit(vec(-5,-2,0),vec(-5,-9,0),vec(3,-9,0),c7,8),BoxInit(-6,-5.5,2,7,c7,0.6,8)]
+Stage9.extend([BoxInit(4.5,-10,3,1,c7,0.7,8),BoxInit(12.5,-10,3,1,c7,0.6,8),BoxInit(0,2,3.5,1.5,c7,0.6,8),BoxInit(10,8,3,1.5,c7,0.6,8)])
+Stage10 = [BoxInit(10,-11,3,8,c8,0,9),BoxInit(0,-2,23,10,c8,0,9),BoxInit(-9,-14.25,5,1.5,c8,0,9),BoxInit(-16,-13,1,1,c8,0,9),BoxInit(-16,-3,1,1,c8,0,9),EndInit(10,3.5,1,1)]
 g = vec(0,-15,0)
 
 
@@ -227,7 +261,7 @@ scene.bind("keydown",on_keydown)
 scene.bind("keyup",on_keyup)
 scene.bind('click keydown', process)
 
-Stages = [Stage1,Stage2,Stage3,Stage4,Stage5,Stage6,Stage7,Stage8]
+Stages = [Stage1,Stage2,Stage3,Stage4,Stage5,Stage6,Stage7,Stage8,Stage9,Stage10]
 
 player = box(pos = vec(0,-12.8,0), size = vec(1,1,0.01), color = color.white)
 player.v = vec(0,0,0)
@@ -239,15 +273,18 @@ player.JumpPMax = 20
 player.MoveVert = False
 player.OnLand = True
 player.CurStage = 0
+player.IsSliding
 
 dt = 0.01
 t = 0
 DJ = DetectCollision()
-StageChange(6)
+StageChange(0)
+
+Game = True
 
 def Normal():
     global DJ
-    if player.MoveVert or player.IsJump:
+    if player.MoveVert or player.IsJump or player.IsSliding:
         DJ = DetectCollision()
         
     if player.WaitForJump and player.JumpP <= player.JumpPMax:
@@ -261,22 +298,41 @@ def Normal():
         elif player.pos.y < -15 + 30 * player.CurStage:
             StageChange(-1)
     
+    if player.IsSliding:
+        player.MoveVert = False
+        player.pos.x = player.pos.x + player.v.x * dt
+        player.v.x = player.v.x * (1-dt)
+        if abs(player.v.x) < 0.5:
+            player.v.x = 0
+            player.color = color.white
+            player.IsSliding = False
+    
     if player.MoveVert:
         player.pos.x = player.pos.x + dt * player.dx * 10
+    
 
     if not DJ and player.OnLand:
         player.OnLand = False
         player.IsJump = True
         player.color = color.red
         player.MoveVert = False
+        player.IsSliding = False
+        if player.WaitForJump:
+            player.pos.y = player.pos.y + player.size.y * 0.5
+            player.size.y = player.size.y * 2
+            player.WaitForJump = False
+            player.IsJump = True
+            player.OnLand = False
+            player.IsSliding = False
+            player.JumpP = 0
+            
 
 def test():
     player.pos = scene.mouse.pos
 
-TE = True
-    
+TE = False
 
-while True:
+while Game:
     rate(100)
     if TE:
         test()
